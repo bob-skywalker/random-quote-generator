5568fd443ab76d0d51bcce4c65bf08596655dca4
bobo <q463250938@gmail.com>
2023-10-22 12:01:12 -0700

    random_quotes generator completed with JSON fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { useState } from 'react'
+import { useEffect } from 'react';
 import './assets/App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState(null);
+  const [author, setAuthor] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const fetchQuotes = () => {
+    fetch('https://api.quotable.io/random?maxLength=200')
+      .then(res => res.json())
+      .then(data => {
+          setQuote(data.content)
+          setAuthor(data.author)
+          })
+      .then(() => setLoading(false))
+      .catch((err) => {
+        console.log("Error fetching the quotes:", err);
+        setLoading(false)
+      })
+  }
+
+  useEffect(fetchQuotes,[])
+
 
   return (
     <>
@@ -10,9 +30,9 @@ function App() {
         <div className='quote-box'>
           <div className='quote-text'>
             <i className='fa fa-quote-left'> </i>
-            <span id='text'> If you’re offered a seat on a rocket ship, don’t ask what seat! Just get on.</span>
+            <span id='text'>{quote}</span>
           </div>
-          <div className='quote-author'> - Grandma Moses </div>
+          <div className='quote-author'> - {author} </div>
           <div className='buttons'>
             <a className='button1' id='main_buttons' target='_blank' href='https://www.google.com'>
               <i class="fa fa-twitter"></i>
@@ -20,13 +40,13 @@ function App() {
             <a className='button2' id='main_buttons' target='_blank'  href='https://www.google.com'>
             <i class="fa fa-tumblr"></i>
             </a>
-            <button className='button3'>New Quote </button>
+            <button className='button3' onClick={fetchQuotes}>New Quote </button>
           </div>
         </div>
       </div>
       <div className="footer">
         by
-        <a href="https://www.bozhong-app.com">  Bo Z</a>
+        <a href="https://www.bozhong-app.com">  Bo Z.</a>
       </div>
     </>
   )
